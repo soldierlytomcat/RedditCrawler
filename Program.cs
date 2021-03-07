6ac51456232aeb4e29bf6151b26e2f76e08d814d
Oaken <oladelesamson37@yahoo.com>
2021-03-07 21:29:50 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running crawler script");
+            ScriptExecutor.run_cmd("", "");
+
+
+            string v = Console.ReadLine();
+        }
+    }
+}
---
+++ ScriptExecutor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RedditCrawler
+{
+    public abstract class ScriptExecutor
+    {
+        public static void run_cmd(string cmd, string args)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            var path = Environment.CurrentDirectory;
+
+            start.FileName = $@"{path}/Reddit Post Crawler/reddit-crawler-env/Scripts/python.exe";
+            var file = $@"{path}/Reddit Post Crawler/reddit-crawler-env/Scripts/praw_scrapper.py";
+            //start.Arguments = string.Format("\"{0}\" \"{1}\"", cmd, file);
+            start.Arguments = string.Format("\"{0}\"", file);
+            start.UseShellExecute = false;
+            start.CreateNoWindow = true;
+            start.RedirectStandardOutput = true;
+            start.RedirectStandardError = true;
+
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    Console.Write(result);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
